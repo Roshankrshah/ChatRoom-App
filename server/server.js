728fd505e23413552756addd969c57ed3bfb3962
Roshan <shahroshan0720@gmail.com>
2023-09-09 13:14:12 +0530

    user name setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
 const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser} = require('./utils/users');
 const app = express();
 
 const server = http.createServer(app);
@@ -16,16 +17,26 @@ const io = socketio(server,{
 const botName = 'ChitChat Bot'
 
 io.on('connection',socket=>{
-    socket.emit('message',formatMessage(botName,'Welcome to ChitChat'));
 
-    socket.broadcast.emit('message',formatMessage(botName,'A user has joined the chat'));
+    socket.on('joinRoom',({username, room})=>{
+
+        const user = userJoin(socket.id,username,room);
+
+        socket.join(user.room);
+
+        socket.emit('message',formatMessage(botName,'Welcome to ChitChat'));
+
+        socket.broadcast.to(user.room).emit('message',formatMessage(botName,`${user.username} has joined the chat`));
+    })
+
+    socket.on('chatMessage',(msg)=>{
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message',formatMessage(user.username,msg));
+    });
 
     socket.on('disconnect',()=>{
         io.emit('message',formatMessage(botName,'A user has left the chat'));
-    });
-
-    socket.on('chatMessage',(msg)=>{
-        io.emit('message',formatMessage('USER',msg));
     });
 });
 
