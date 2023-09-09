bf5230259920462dc9dbd81fc1ca6519906241ea
Roshan <shahroshan0720@gmail.com>
2023-09-09 12:21:34 +0530

    send message setup

--- client/script.js
+++ client/script.js
@@ -1,5 +1,27 @@
+const chatForm = document.getElementById('chat-form');
+
 const socket = io('http://localhost:3000');
 
 socket.on('message',(message)=>{
     console.log(message);
+    outputMessage(message);
 });
+
+chatForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const msg = e.target.elements.msg.value;
+    
+    socket.emit('chatMessage',msg);
+});
+
+function outputMessage(message){
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `
+        <p class="meta">Brad <span>9.12</span></p>
+        <p class="text">
+            ${message}
+        </p>`;
+    
+    document.querySelector('.chat-messages').appendChild(div);
+}
--- server/server.js
+++ server/server.js
@@ -13,8 +13,17 @@ const io = socketio(server,{
 });
 
 io.on('connection',socket=>{
-    console.log('New ws connection');
     socket.emit('message','Welcome to ChitChat');
+
+    socket.broadcast.emit('message','A user has joined the chat');
+
+    socket.on('disconnect',()=>{
+        io.emit('message','A user has left the chat');
+    });
+
+    socket.on('chatMessage',(message)=>{
+        io.emit('message',message);
+    });
 });
 
 server.listen(3000,()=>{
