23330527d46e340d4bdf40f9d643d5d4c1f51b77
Roshan <shahroshan0720@gmail.com>
2023-09-09 11:25:05 +0530

    home page structure setup

--- client/script.js
+++ client/script.js
@@ -1,1 +1,7 @@
 const socket = io('http://localhost:3000');
+
+socket.on('connect',()=>{
+    console.log(`you connected with id: ${socket.id}`);
+});
+
+socket.emit('custom-event',10,'hi',{a:'a'});
